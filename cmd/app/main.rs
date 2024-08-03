3e9af52f02db160958ec99ad501f454834c95ffc
bquentin <bquentin@protonmail.com>
2024-08-03 11:09:32 +0200

    add graphql

--- cmd/app/main.rs
+++ cmd/app/main.rs
@@ -1,124 +1,130 @@
 use dotenv::dotenv;
-use std::env;
-use actix_web::{App, HttpServer, HttpResponse, Responder};
-use actix_web::web::{Data, Json, Path};
-use sea_orm::{Database, EntityTrait, Set, ActiveModelTrait, DatabaseConnection};
+
+use actix_web::{web::Data, App, HttpServer};
+use async_graphql::{Context, EmptySubscription, Object, Schema, SimpleObject, InputObject};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use sea_orm::{Database, EntityTrait, QueryFilter, Set, DatabaseConnection};
 use sea_orm::prelude::*;
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
+use std::env;
 use template::internal::entities::collection;
 use template::internal::entities::field;
+use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, SimpleObject, InputObject)]
 struct CreateCollection {
     name: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, SimpleObject, InputObject)]
 struct CreateField {
     value_type: String,
     value_name: String,
 }
 
+#[derive(SimpleObject)]
+struct Collection {
+    id: Uuid,
+    name: String,
+}
 
+#[derive(SimpleObject)]
+struct Field {
+    id: Uuid,
+    collection_id: Uuid,
+    value_type: String,
+    value_name: String,
+}
 
-#[actix_web::get("/collections")]
-async fn get_collections(db: Data<DatabaseConnection>) -> impl Responder {
-    match collection::Entity::find().find_with_related(field::Entity).all(db.get_ref()).await {
-        Ok(entries) => {
-            HttpResponse::Ok().json(entries)
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn collections(&self, ctx: &Context<'_>) -> async_graphql::Result<Vec<Collection>> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        let collections = collection::Entity::find().all(db).await?;
+        Ok(collections.into_iter().map(|c| Collection {
+            id: c.id,
+            name: c.name,
+        }).collect())
+    }
+
+    async fn collection(&self, ctx: &Context<'_>, id: Uuid) -> async_graphql::Result<Option<Collection>> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        if let Some(c) = collection::Entity::find_by_id(id).one(db).await? {
+            Ok(Some(Collection {
+                id: c.id,
+                name: c.name,
+            }))
+        } else {
+            Ok(None)
         }
-        Err(_) => {
-            HttpResponse::InternalServerError().finish()
-        }
+    }
+
+    async fn fields(&self, ctx: &Context<'_>, collection_id: Uuid) -> async_graphql::Result<Vec<Field>> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        let fields = field::Entity::find()
+            .filter(field::Column::CollectionId.eq(collection_id))
+            .all(db)
+            .await?;
+        Ok(fields.into_iter().map(|f| Field {
+            id: f.id,
+            collection_id: f.collection_id,
+            value_type: f.value_type,
+            value_name: f.value_name,
+        }).collect())
     }
 }
 
+struct MutationRoot;
 
-#[actix_web::post("/collections")]
-async fn create_collection(db: Data<DatabaseConnection>, item: Json<CreateCollection>) -> impl Responder {
-    let new_uuid = Uuid::new_v4();
-    let new_entry = collection::ActiveModel {
-        id: Set(new_uuid),
-        name: Set(item.name.clone()),
-    };
+#[Object]
+impl MutationRoot {
+    async fn create_collection(&self, ctx: &Context<'_>, name: String) -> async_graphql::Result<Collection> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        let new_uuid = Uuid::new_v4();
+        let new_entry = collection::ActiveModel {
+            id: Set(new_uuid),
+            name: Set(name),
+        };
 
-    match new_entry.insert(db.get_ref()).await {
-        Ok(_) => {
-            match collection::Entity::find_by_id(new_uuid).one(db.get_ref()).await {
-                Ok(Some(entry)) => {
-                    HttpResponse::Ok().json(entry)
-                },
-                Ok(None) => {
-                    HttpResponse::NotFound().finish()
-                },
-                Err(_) => {
-                    HttpResponse::InternalServerError().finish()
-                }
-            }
-        },
-        Err(_) => {
-            HttpResponse::InternalServerError().finish()
-        }
+        let c = new_entry.insert(db).await?;
+        Ok(Collection {
+            id: c.id,
+            name: c.name,
+        })
     }
-}
 
-#[actix_web::get("/collections/{id}")]
-async fn get_collection(db: Data<DatabaseConnection>, id: Path<Uuid>) -> impl Responder {
-    match collection::Entity::find_by_id(*id).one(db.get_ref()).await {
-        Ok(Some(entry)) => {
-            HttpResponse::Ok().json(entry)
-        },
-        Ok(None) => {
-            HttpResponse::NotFound().finish()
-        },
-        Err(_) => {
-            HttpResponse::InternalServerError().finish()
-        }
+    async fn create_field(&self, ctx: &Context<'_>, collection_id: Uuid, value_type: String, value_name: String) -> async_graphql::Result<Field> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        let new_uuid = Uuid::new_v4();
+
+        let new_entry = field::ActiveModel {
+            id: Set(new_uuid),
+            collection_id: Set(collection_id),
+            value_type: Set(value_type),
+            value_name: Set(value_name),
+        };
+
+        let f = new_entry.insert(db).await?;
+        Ok(Field {
+            id: f.id,
+            collection_id: f.collection_id,
+            value_type: f.value_type,
+            value_name: f.value_name,
+        })
     }
-}
 
-#[actix_web::get("/collections/{id}/fields")]
-async fn get_fields(db: Data<DatabaseConnection>, id: Path<Uuid>) -> impl Responder {
-    match field::Entity::find().filter(field::Column::CollectionId.eq(*id)).all(db.get_ref()).await {
-        Ok(entries) => {
-            HttpResponse::Ok().json(entries)
-        }
-        Err(_) => {
-            HttpResponse::InternalServerError().finish()
-        }
+    async fn delete_collection(&self, ctx: &Context<'_>, id: Uuid) -> async_graphql::Result<bool> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        let res = collection::Entity::delete_by_id(Uuid::from(id)).exec(db).await?;
+        Ok(res.rows_affected > 0)
     }
-}
 
-#[actix_web::post("/collections/{id}/fields")]
-async fn create_field(db: Data<DatabaseConnection>, id: Path<Uuid>, item: Json<CreateField>) -> impl Responder {
-    let new_uuid = Uuid::new_v4(); 
-
-    let new_entry = field::ActiveModel {
-        id: Set(new_uuid),
-        collection_id: Set(*id),
-        value_type: Set(item.value_type.clone()),
-        value_name: Set(item.value_name.clone()),
-    };
-    
-    match new_entry.insert(db.get_ref()).await {
-        Ok(_) => {
-            match field::Entity::find_by_id(new_uuid).one(db.get_ref()).await {
-                Ok(Some(entry)) => {
-                    HttpResponse::Ok().json(entry)
-                },
-                Ok(None) => {
-                    HttpResponse::NotFound().finish()
-                },
-                Err(_) => {
-                    HttpResponse::InternalServerError().finish()
-                }
-            }
-        },
-        Err(_) => {
-            HttpResponse::InternalServerError().finish()
-        }
+    async fn delete_field(&self, ctx: &Context<'_>, id: Uuid) -> async_graphql::Result<bool> {
+        let db = ctx.data::<DatabaseConnection>().unwrap();
+        let res = field::Entity::delete_by_id(Uuid::from(id)).exec(db).await?;
+        Ok(res.rows_affected > 0)
     }
 }
 
@@ -138,16 +144,27 @@ async fn main() -> std::io::Result<()> {
 
     let db = Database::connect(&connection_string).await.unwrap();
 
+    let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
+        .data(db)
+        .finish();
+
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(db.clone()))
-            .service(get_collections)
-            .service(create_collection)
-            .service(get_fields)
-            .service(create_field)
+            .app_data(Data::new(schema.clone()))
+            .service(actix_web::web::resource("/graphql").guard(actix_web::guard::Post()).to(graphql_handler))
+            .service(actix_web::web::resource("/graphql").guard(actix_web::guard::Get()).to(graphql_playground))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+async fn graphql_handler(schema: Data<Schema<QueryRoot, MutationRoot, EmptySubscription>>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn graphql_playground() -> actix_web::Result<actix_web::HttpResponse> {
+    let playground = async_graphql::http::GraphiQLSource::build().endpoint("/graphql").finish();
+    Ok(actix_web::HttpResponse::Ok().content_type("text/html").body(playground))
 }
 
--- src/internal/entities/collection.rs
+++ src/internal/entities/collection.rs
@@ -1,4 +1,5 @@
-use sea_orm::entity::prelude::*;
+use async_trait::async_trait;
+use sea_orm::{entity::prelude::*, ActiveValue, DbBackend, Statement};
 use uuid::Uuid;
 use serde::{Deserialize, Serialize};
 
@@ -22,4 +23,49 @@ impl Related<super::field::Entity> for Entity {
     }
 }
 
-impl ActiveModelBehavior for ActiveModel {}
+#[async_trait]
+impl ActiveModelBehavior for ActiveModel {
+    async fn before_save<C>(mut self, db: &C, _insert: bool) -> Result<Self, DbErr>
+    where
+        C: ConnectionTrait,
+    {
+        if let ActiveValue::Set(ref current_name) = self.name {
+            let sanitized_name = current_name.replace("'", "''");
+
+            // Check if the table exists
+            let check_table_stmt = format!(
+                "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '{}')",
+                sanitized_name
+            );
+            let result = db.query_one(Statement::from_string(DbBackend::Postgres, check_table_stmt)).await?;
+
+            if let Some(row) = result {
+                let exists: bool = row.try_get("", "exists")?;
+                if exists {
+                    // Save the current name in old_name if the table exists
+                    self.old_name = ActiveValue::Set(Some(sanitized_name.clone()));
+                } else {
+                    // Table does not exist, create it
+                    let create_table_stmt = format!(
+                        "CREATE TABLE {} (id SERIAL PRIMARY KEY, name VARCHAR(255), other_field VARCHAR(255))",
+                        sanitized_name
+                    );
+                    db.execute(Statement::from_string(DbBackend::Postgres, create_table_stmt)).await?;
+                }
+            }
+
+            // Rename the table if old_name is set and different from current name
+            if let ActiveValue::Set(Some(ref old_name)) = self.old_name {
+                if old_name != &sanitized_name {
+                    let rename_table_stmt = format!(
+                        "ALTER TABLE {} RENAME TO {}",
+                        old_name, sanitized_name
+                    );
+                    db.execute(Statement::from_string(DbBackend::Postgres, rename_table_stmt)).await?;
+                }
+            }
+        }
+
+        Ok(self)
+    }
+}
