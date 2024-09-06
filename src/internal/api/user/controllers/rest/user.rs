62beee9d064e227ee7dff748b3d4db073b74877a
Quentin Besse <qbesse@clever-age.com>
2024-09-06 17:18:47 +0200

    BA-003 : Add error and trace in rest in rest api

--- src/internal/api/user/controllers/rest/user.rs
+++ src/internal/api/user/controllers/rest/user.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse, Responder};
+use log::{error, trace};
 use sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -19,32 +20,74 @@ pub struct UpdateUserInput {
 }
 
 pub async fn get_user(db: web::Data<DatabaseConnection>, id: web::Path<Uuid>) -> impl Responder {
-    match UserServiceImpl::get_user(db.get_ref(), id.into_inner()).await {
-        Ok(Some(user)) => HttpResponse::Ok().json(user),
-        Ok(None) => HttpResponse::NotFound().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    let user_id = id.into_inner();
+    trace!("Attempting to fetch user with id: {}", user_id);
+
+    match UserServiceImpl::get_user(db.get_ref(), user_id).await {
+        Ok(Some(user)) => {
+            trace!("User found: {:?}", user);
+            HttpResponse::Ok().json(user)
+        },
+        Ok(None) => {
+            trace!("User with id {} not found", user_id);
+            HttpResponse::NotFound().finish()
+        },
+        Err(e) => {
+            error!("Failed to get user with id {}: {:?}", user_id, e);
+            HttpResponse::InternalServerError().finish()
+        },
     }
 }
 
 pub async fn create_user(db: web::Data<DatabaseConnection>, user_data: web::Json<CreateUserInput>) -> impl Responder {
-    match UserServiceImpl::create_user(db.get_ref(), user_data.username.clone(), user_data.email.clone(), user_data.password.clone()).await {
-        Ok(user) => HttpResponse::Created().json(user),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    let user_data = user_data.into_inner();
+    trace!("Attempting to create user with username: '{}', email: '{}'", user_data.username, user_data.email);
+
+    match UserServiceImpl::create_user(db.get_ref(), user_data.username.clone(), user_data.email.clone(), user_data.password).await {
+        Ok(user) => {
+            trace!("User created successfully: {:?}", user);
+            HttpResponse::Created().json(user)
+        },
+        Err(e) => {
+            error!("Failed to create user with username '{}', email '{}': {:?}", user_data.username, user_data.email, e);
+            HttpResponse::InternalServerError().finish()
+        },
     }
 }
 
 pub async fn update_user(db: web::Data<DatabaseConnection>, id: web::Path<Uuid>, user_data: web::Json<UpdateUserInput>) -> impl Responder {
-    match UserServiceImpl::update_user(db.get_ref(), id.into_inner(), user_data.username.clone(), user_data.email.clone(), user_data.password.clone()).await {
-        Ok(user) => HttpResponse::Ok().json(user),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    let user_id = id.into_inner();
+    let user_data = user_data.into_inner();
+    trace!("Attempting to update user with id: '{}', username: '{:?}', email: '{:?}'", user_id, user_data.username, user_data.email);
+
+    match UserServiceImpl::update_user(db.get_ref(), user_id, user_data.username.clone(), user_data.email.clone(), user_data.password).await {
+        Ok(user) => {
+            trace!("User updated successfully: {:?}", user);
+            HttpResponse::Ok().json(user)
+        },
+        Err(e) => {
+            error!("Failed to update user with id '{}', username '{:?}', email '{:?}': {:?}", user_id, user_data.username, user_data.email, e);
+            HttpResponse::InternalServerError().finish()
+        },
     }
 }
 
 pub async fn delete_user(db: web::Data<DatabaseConnection>, id: web::Path<Uuid>) -> impl Responder {
-    match UserServiceImpl::delete_user(db.get_ref(), id.into_inner()).await {
-        Ok(true) => HttpResponse::Ok().finish(),
-        Ok(false) => HttpResponse::NotFound().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+    let user_id = id.into_inner();
+    trace!("Attempting to delete user with id: {}", user_id);
+
+    match UserServiceImpl::delete_user(db.get_ref(), user_id).await {
+        Ok(true) => {
+            trace!("User with id {} deleted successfully", user_id);
+            HttpResponse::Ok().finish()
+        },
+        Ok(false) => {
+            trace!("User with id {} not found for deletion", user_id);
+            HttpResponse::NotFound().finish()
+        },
+        Err(e) => {
+            error!("Failed to delete user with id '{}': {:?}", user_id, e);
+            HttpResponse::InternalServerError().finish()
+        },
     }
 }
-
