53fa0da3bbfa0de498098871917afa60b152e280
Quentin Besse <qbesse@clever-age.com>
2024-09-13 15:55:42 +0200

    BA-020 : delete frontend

--- frontend/src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-
-export const handle: Handle = async ({ event, resolve }) => {
-  return resolve(event, {
-    filterSerializedResponseHeaders: (name) => name === 'content-type'
-  });
-};
